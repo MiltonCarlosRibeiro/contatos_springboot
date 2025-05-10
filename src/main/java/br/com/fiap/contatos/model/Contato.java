6ff5cebcd6f45ce6d095f6c5419c4e0a0e27f30c
MiltonCarlosRibeiro <milton.carlos.dev@gmail.com>
2025-05-10 10:38:47 -0300

    Ajust

--- src/main/java/br/com/fiap/contatos/model/Contato.java
+++ src/main/java/br/com/fiap/contatos/model/Contato.java
@@ -1,7 +1,68 @@
 package br.com.fiap.contatos.model;
 
+import jakarta.persistence.Entity;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
 public class Contato {
 
-    private Long idD
+    private Long id;
 
+    private String nome;
+
+    private String email;
+
+    private LocalDate dataNascimento;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contato contato = (Contato) o;
+        return Objects.equals(id, contato.id) && Objects.equals(nome, contato.nome) && Objects.equals(email, contato.email) && Objects.equals(dataNascimento, contato.dataNascimento);
+    }
+
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, email, dataNascimento);
+    }
 }
+
